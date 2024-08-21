53280b289cc9fb7fe3116a299afc84e78a999fba
eveporcello <eve@moonhighway.com>
2024-08-21 09:36:01 -0700

    Adds 05_05e

--- next-react-project/src/app/layout.js
+++ next-react-project/src/app/layout.js
@@ -23,6 +23,9 @@ function Header() {
           <div>
             <Link href="/hotels">Hotels</Link>
           </div>
+          <div>
+            <Link href="/contact">Contact Us</Link>
+          </div>
         </nav>
       </div>
     </header>
