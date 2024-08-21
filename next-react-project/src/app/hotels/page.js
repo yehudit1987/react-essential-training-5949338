efc362c87d19d11f24e01d23700ac3cfe99b23e7
eveporcello <eve@moonhighway.com>
2024-08-21 09:43:12 -0700

    Adds 06_03e

--- next-react-project/src/app/hotels/page.js
+++ next-react-project/src/app/hotels/page.js
@@ -1,17 +1,10 @@
+import HotelBlock from "./HotelBlock";
+
 async function getData() {
   const res = await fetch(
     "https://snowtooth-hotel-api.fly.dev"
   );
   return res.json();
-}
-
-function HotelBlock({ name, capacity }) {
-  return (
-    <div>
-      <h2>{name}</h2>
-      <p>{capacity}</p>
-    </div>
-  );
 }
 
 export default async function Page() {
@@ -24,6 +17,7 @@ export default async function Page() {
           {data.map((hotel) => (
             <HotelBlock
               key={hotel.id}
+              id={hotel.id}
               name={hotel.name}
               capacity={hotel.capacity}
             />
