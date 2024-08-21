0e5afd159bf5f059deb7ee341ab4f716efbfee5b
eveporcello <eve@moonhighway.com>
2024-08-21 09:07:29 -0700

    Adds 03_04e

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -23,23 +23,28 @@ const dishObjects = items.map((dish, i) => ({
 
 function Main({ dishes }) {
   return (
-    <main>
-      <img
-        src={chef}
-        height={200}
-        alt="A photo of a smiling chef owner"
-      />
-      <ul>
-        {dishes.map((dish) => (
-          <li
-            key={dish.id}
-            style={{ listStyleType: "none" }}
-          >
-            {dish.title}
-          </li>
-        ))}
-      </ul>
-    </main>
+    <>
+      <div>
+        <h2>Welcome to this beautiful restaurant!</h2>
+      </div>
+      <main>
+        <img
+          src={chef}
+          height={200}
+          alt="A photo of a smiling chef owner"
+        />
+        <ul>
+          {dishes.map((dish) => (
+            <li
+              key={dish.id}
+              style={{ listStyleType: "none" }}
+            >
+              {dish.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
 
