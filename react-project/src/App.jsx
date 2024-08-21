3ee5672c50a405ff7eee4135ec7d9b4c6a2d4b40
eveporcello <eve@moonhighway.com>
2024-08-21 09:24:32 -0700

    Adds 04_06e

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import "./App.css";
 import chef from "./images/chef.jpg";
 
@@ -56,11 +56,17 @@ function Main({ dishes, openStatus, onStatus }) {
 }
 
 function App() {
-  // const [status, setStatus] = useState(true);
   const [status, toggle] = useReducer(
     (status) => !status,
     true
   );
+
+  useEffect(() => {
+    console.log(
+      `The restaurant is ${status ? "open" : "closed"}.`
+    );
+  }, [status]);
+
   return (
     <div>
       <h1>
