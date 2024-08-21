49c23d1bc4ca40d76cd2e9f48847e6cac3c593b9
eveporcello <eve@moonhighway.com>
2024-08-21 09:03:19 -0700

    Adds 03_02e

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -11,14 +11,22 @@ function Header({ name, year }) {
 const items = [
   "Macaroni and Cheese",
   "Salmon with Potatoes",
-  "Tofu with Vegetables"
+  "Tofu with Vegetables",
+  "Minestrone Soup"
 ];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish
+}));
 
 function Main({ dishes }) {
   return (
     <ul>
       {dishes.map((dish) => (
-        <li style={{ listStyleType: "none" }}>{dish}</li>
+        <li key={dish.id} style={{ listStyleType: "none" }}>
+          {dish.title}
+        </li>
       ))}
     </ul>
   );
@@ -28,7 +36,7 @@ function App() {
   return (
     <div>
       <Header name="Alex" year={new Date().getFullYear()} />
-      <Main dishes={items} />
+      <Main dishes={dishObjects} />
     </div>
   );
 }
