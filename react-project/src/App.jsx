378d9f5c7a282560eb201c53ad531068f3843c9d
Yehudit Kerido <yehudit.kerido@nokia.com>
2024-11-20 13:23:04 +0200

    Adding unique keys to objects in list

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,5 +1,9 @@
 import "./App.css";
-
+/* 
+  In the previous example (02_06e) we were getting a warning in the web console,
+  about the items that have no unique key for each item.
+  in this example we will see how to solve that warning. 
+   */
 function Header({ name, year }) {
   return (
     <header>
@@ -15,11 +19,17 @@ const items = [
   "Minestrone Soup"
 ];
 
+/* 
+  Here we will create dishObjects. 
+  dishObjects is a list of objects. 
+  Each object is construct of 2 key values pair of id and title. 
+*/
 const dishObjects = items.map((dish, i) => ({
   id: i,
   title: dish
 }));
 
+// Now in Main we reference the 2 keys of each object but ofcourse displaying just the title. 
 function Main({ dishes }) {
   return (
     <ul>
@@ -32,6 +42,7 @@ function Main({ dishes }) {
   );
 }
 
+// Now in App component we will pass dishObjects to Main. 
 function App() {
   return (
     <div>
