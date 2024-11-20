a031aca276e537a6da69ec0caa5eb8e1c19410c7
Yehudit Kerido <yehudit.kerido@nokia.com>
2024-11-20 12:46:31 +0200

    JSX syntax - basics

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,15 +1,21 @@
+// A react component is a function that return a little bit of UI. 
 import "./App.css";
 
+// This is the header component
 function Header() {
   return (
+    //These are jsx tags but we can think about them as html tags. 
     <header>
       <h1>Eve's Kitchen</h1>
     </header>
   );
 }
 
+//This is the app component
 function App() {
   return (
+    // We can render a component (such as header) as part of other component (=app). 
+    // In case of multiple, we need to wrap them with <div> tags. 
     <div>
       <Header />
       <main>
