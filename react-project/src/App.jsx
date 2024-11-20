bea3b0941131c4daa9f5b855800450860fc5a2dc
Yehudit Kerido <yehudit.kerido@nokia.com>
2024-11-20 13:11:16 +0200

    Lists as props

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -11,9 +11,13 @@ function Header({ name, year }) {
 const items = [
   "Macaroni and Cheese",
   "Salmon with Potatoes",
-  "Tofu with Vegetables"
+  "Tofu with Vegetables",
 ];
 
+/*
+   Main is a component that get dishes which is a list of items.
+   It iterates the items and for each dish it display the item. 
+   Here we also use css styling. */
 function Main({ dishes }) {
   return (
     <ul>
@@ -24,6 +28,10 @@ function Main({ dishes }) {
   );
 }
 
+/*
+  In the App component, we render Main components with dishes prop that get the value of items.
+  We can see the initialization of items above.
+ */
 function App() {
   return (
     <div>
