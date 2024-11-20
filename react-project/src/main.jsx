49016a65e794ee7b3b3b209cfe1b7d552d3a02a4
Yehudit Kerido <yehudit.kerido@nokia.com>
2024-11-20 14:37:04 +0200

    Destructing arrays

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,7 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+/* 
+  In this demo we just do a previous to the next video. 
+  We are demonstarting a way to create list with key and access those values by the keys. 
+*/
 const [, , third] = ["giraffe", "zebra", "bear"];
-console.log(third);
+console.log(third);  // That will print to console - "bear"
 
 createRoot(document.getElementById("root")).render(<App />);
