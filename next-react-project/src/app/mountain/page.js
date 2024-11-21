1b1d5578a9ee7117883ea38c5c7f0556399f9c20
Yehudit Kerido <yehudit.kerido@nokia.com>
2024-11-21 09:20:33 +0200

    App router - Next.js

--- next-react-project/src/app/mountain/page.js
+++ next-react-project/src/app/mountain/page.js
@@ -1,3 +1,7 @@
+/*
+  This function defined with export default since other file is about to import it and use it or use a lin to navigate to. 
+  In our case /src/app/layout.js is using Links in the Header component to navigate that route. 
+*/
 export default function Page() {
   return (
     <main>
